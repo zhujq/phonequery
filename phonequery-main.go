7328b3ab85af0123ec61e20307bd0cabe50f9421
zhujq <=>
2021-10-07 23:50:09 +0800

    url correct

--- phonequery-main.go
+++ phonequery-main.go
@@ -35,23 +35,6 @@ func NewApp() *App {
 
 var app = NewApp()
 
-/*
-func init() {                                         //初始，日志文件生成
-	file := "./" +"logindex"+ ".txt"
-	logFile, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0766)
-	if err != nil {
-			panic(err)
-	}
-	defer logFile.Close()
-	log.SetOutput(logFile) // 将文件设置为log输出的文件
-//	mw := io.MultiWriter(os.Stdout,logFile) //同时输出到文件和控制台
-//  log.SetOutput(mw)
-	log.SetPrefix("[wechat-index]")
-	log.SetFlags(log.LstdFlags | log.Lshortfile | log.LUTC)
-	return
-}
-*/
-
 
 func main() {
 	var err error
@@ -83,7 +66,7 @@ func indexHandler(ctx dotweb.Context) error {
 
 	if phonenum == "" {
 		log.Println("ERROR: 没有提供电话号码")
-		return ctx.WriteString("欢迎使用中国电话号码查询系统，请在网址后输入phonenum=电话号码查询，如https://phone.zhujq.ga/?phonenum=13988888888")
+		return ctx.WriteString("欢迎使用中国电话号码查询系统，请在网址后输入phonenum=电话号码查询，如https://phone.zhujq.ml/?phonenum=13988888888")
 	}
 
 	var message = ResBody{
